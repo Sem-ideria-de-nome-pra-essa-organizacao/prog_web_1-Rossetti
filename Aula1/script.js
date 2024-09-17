684070594bf2f1d1f2d1361cf7c64bf39bbcbdbd
Lucasboina <lucasmmantelli@gmail.com>
2024-09-17 17:15:55 -0300

    parte 2 aula 1

--- Aula1/script.js
+++ Aula1/script.js
@@ -1,12 +1,41 @@
-const alertButton = document.getElementById("alert-button");
-const changeColorButton = document.getElementById("change-color-button");
-const text = document.getElementById("text");
+{
+    const alertButton = document.getElementById("alert-button");
+    const changeColorButton = document.getElementById("change-color-button");
+    const hoverButton = document.getElementById("hover-button");
+    const text = document.getElementById("text");
+    const animationButton = document.getElementById("animation-button");
 
-alertButton.addEventListener("click", () => window.alert("AAAAA"));
-console.log(alertButton);
+    alertButton.addEventListener("click", () => window.alert("AAAAA"));
 
-changeColorButton.addEventListener("click", () => {
-    console.log("teste");
-    text.style.backgroundColor = "#000";
-    text.style.color = "#fff";
-});
+    hoverButton.addEventListener("mousemove", () => {
+        text.style.backgroundColor = "#ff0";
+        text.style.color = "#000";
+        text.innerHTML = "HOVER";
+    });
+
+    changeColorButton.addEventListener("click", () => {
+        text.style.backgroundColor = "#000";
+        text.style.color = "#fff";
+        text.innerHTML = "Click";
+    });
+    animationButton.addEventListener("click", () => {
+        Animation();
+    });
+
+    const Animation = () => {
+        let id = null;
+        const elem = document.getElementById("animate");
+        let pos = 0;
+        clearInterval(id);
+        id = setInterval(frame, 5);
+        function frame() {
+            if (pos == 1500) {
+                clearInterval(id);
+            } else {
+                pos++;
+                elem.style.top = pos + "px";
+                elem.style.left = pos + pos + pos + "px";
+            }
+        }
+    };
+}
